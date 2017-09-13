a093b9ad76c57d3f789fc4d775d8ccd7e315c8c3
Mario J. Juha <mario.juha@unisabana.edu.co>
2017-09-13 14:43:20 -0500

    fixed a bug in computeSideLoad

--- computeSideLoad.m
+++ computeSideLoad.m
@@ -12,6 +12,8 @@ h = convectionLoad(el,6); % coefficient
 Ta = convectionLoad(el,7); % ambient temperature
 
 % loop over gauss points
+% below we are going to use the same interchangably r,s and t.
+% But be aware that this is only for simplifying the program
 if face == 1 % local nodes 1-2-3
     r = gp(1);
     s = gp(2);
@@ -19,49 +21,48 @@ if face == 1 % local nodes 1-2-3
     Nshape = [ 1-r-s, r, s, 0 ];
     N_r = [ -1, 1, 0, 0 ];
     N_s = [ -1, 0, 1, 0 ];
-    N_t = [0, 0, 0, 0];
+    % N_t = [0, 0, 0, 0];
 elseif face == 2 % local nodes 1-2-4
     r = gp(1);
-    t = gp(2);
+    s = gp(2);
     % s = 0
-    Nshape = [ 1-r-t, r, 0, t ];
+    Nshape = [ 1-r-s, r, 0, s ];
     N_r = [ -1 ,1, 0, 0 ];
-    N_s = [0, 0, 0, 0];
-    N_t = [ -1 ,1, 0, 1 ];
-elseif edge == 3 % local nodes 1-3-4
+    % N_s = [0, 0, 0, 0];
+    N_s = [ -1 ,1, 0, 1 ];
+elseif face == 3 % local nodes 1-3-4
     % r = 0
-    s = gp(1);
-    t = gp(2);
-    Nshape = [ 1-s-t, 0, s, t ];
-    N_r = [0, 0, 0, 0];
-    N_s = [ -1, 0, 1, 0 ];
-    N_t = [ -1, 0, 0, 1 ];
-elseif edge == 4 % local nodes 2-3-4
+    r = gp(1);
+    s = gp(2);
+    Nshape = [ 1-r-s, 0, r, s ];
+    % N_r = [0, 0, 0, 0];
+    N_r = [ -1, 0, 1, 0 ];
+    N_s = [ -1, 0, 0, 1 ];
+elseif face == 4 % local nodes 2-3-4
     % t = 1 - r - s
     r = gp(1);
     s = gp(2);
     Nshape = [ 0, r, s, 1-r-s ];
     N_r = [ 0, 1, 0, -1 ];
     N_s = [ 0, 0, 1, -1 ];
-    N_t = [ 0, 0, 0, 0 ];
+    % N_t = [ 0, 0, 0, 0 ];
 else
     error('Wrong face, check input!');
 end
 
 x_r = N_r * xe(:,1);
 x_s = N_s * xe(:,1);
-x_t = N_t * xe(:,1);
 %
 y_r = N_r * xe(:,2);
 y_s = N_s * xe(:,2);
-y_t = N_t * xe(:,2);
 %
 z_r = N_r * xe(:,3);
 z_s = N_s * xe(:,3);
-z_t = N_t * xe(:,3);
 
-jacobian = [x_r, x_s, x_t;  y_r, y_s, y_t; z_r, z_s, z_t];
-jac = det(jacobian);
+A = [x_r, y_r, z_r];
+B = [x_s, y_s, z_s];
+
+jac = 0.5*norm(cross(A,B));
 
 % check jacobian
 if jac < 1.0e-14
@@ -70,4 +71,5 @@ end
 
 fe = Nshape' * (h*Ta) * w * jac;
 ke = Nshape' * h * Nshape * w * jac;
+
 end
--- examples/cylinder/cylinder.geo
+++
@@ -1,7 +0,0 @@
-//+
-SetFactory("OpenCASCADE");
-Cylinder(1) = {0, 0, 0, 0, 0.01, 0, 0.005, 2*Pi};
-//+
-Physical Volume("mat1") = {1};
-//+
-Physical Surface("convLoad") = {2, 1, 3};
